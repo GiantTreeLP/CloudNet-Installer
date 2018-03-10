dcf508c0f162162c586d3a134a9b2380463f0542
Marvin <webmaster@gianttree.de>
2018-03-10 03:28:36 +0100

    Don't break lines where it is useless

--- install.sh
+++ install.sh
@@ -93,8 +93,7 @@ if [ $EUID -ne 0 ]; then
 fi
 
 echo "Welcome to the CloudNet installer for version 2.1Pv30"
-echo "This script will download CloudNet and it's dependencies,"
-echo "so that you can run it right away."
+echo "This script will download CloudNet and it's dependencies, so that you can run it right away."
 sleep 1
 
 echo "Downloading dependencies..."
