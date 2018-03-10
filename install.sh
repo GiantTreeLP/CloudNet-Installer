d8c6391a73b5d2685e087f5566dd5a4ef7c7ecdb
Marvin <webmaster@gianttree.de>
2018-03-10 03:21:16 +0100

    Check if Java is already installed

--- install.sh
+++ install.sh
@@ -22,6 +22,10 @@ install_package() {
 }
 
 install_java() {
+	if install_package 'openjdk-8-jre-headless'; then
+		return
+	fi
+
 	if [ -f "/etc/os-release" ]; then
 		(
 			source "/etc/os-release"
@@ -30,15 +34,18 @@ install_java() {
 			if [ "$ID" = "debian" ]; then
 				# Handle Debian 8
 				if [ "$VERSION_ID" = "8" ]; then
+					echo "Found Debian 8, using jessie-backports of Java 8"
 					echo "deb http://deb.debian.org/debian jessie-backports main" > "/etc/apt/sources.list.d/jessie-backports.list"
 					update_package_cache
 					install_package 'openjdk-8-jre-headless' '-t' 'jessie-backports'
 					return
 				elif [ "$VERSION_ID" > "8" ]; then
+					echo "Found modern Debian, Java 8 should be in the official sources"
 					install_package 'openjdk-8-jre-headless'
 					return
 				else
 					echo "Unsupported version of Debian."
+					echo "You can try manually installing Java using the 'openjdk-8-jre-headless' package."
 					echo "Aborting..."
 					exit 1
 				fi
@@ -115,9 +122,10 @@ chmod u+x "CloudNet-Master/start.sh" "CloudNet-Wrapper/start.sh"
 check_incompatibilities
 
 echo "Cleaning up..."
-# rm "./pacapt" "cloudnet.zip" "$0"
+rm "./pacapt" "cloudnet.zip"
 
 echo "Installation successful."
 echo "Enjoy the Cloud Network Environment Technology CloudNet"
 echo "For questions regarding this script, head over to the official Discord:"
 echo "https://discord.gg/CPCWr7w"
+echo -n
